71a5305ff7e4a73877afa8d590c0b90723b3c7fc
DieWaffles <m240576@usna.edum240576@usna.edu>
2023-02-16 08:49:52 -0500

    newly coded stuff

--- app/src/main/java/edu/usna/mobileos/a1_braemerandrew/MainActivity.kt
+++ app/src/main/java/edu/usna/mobileos/a1_braemerandrew/MainActivity.kt
@@ -2,10 +2,61 @@ package edu.usna.mobileos.a1_braemerandrew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    //var textView: TextView = findViewById(R.id.viewText)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val butZero: Button = findViewById(R.id.zeroBut)
+        butZero.setOnClickListener(this)
+        val butOne: Button = findViewById(R.id.oneBut)
+        butOne.setOnClickListener(this)
+        val butTwo: Button = findViewById(R.id.twoBut)
+        butTwo.setOnClickListener(this)
+        val butThree: Button = findViewById(R.id.threeBut)
+        butThree.setOnClickListener(this)
+        val butFour: Button = findViewById(R.id.fourBut)
+        butFour.setOnClickListener(this)
+        val butFive: Button = findViewById(R.id.fiveBut)
+        butFive.setOnClickListener(this)
+        val butSix: Button = findViewById(R.id.sixBut)
+        butSix.setOnClickListener(this)
+        val butSeven: Button = findViewById(R.id.sevenBut)
+        butSeven.setOnClickListener(this)
+        val butEight: Button = findViewById(R.id.eightBut)
+        butEight.setOnClickListener(this)
+        val butNine: Button = findViewById(R.id.nineBut)
+        butNine.setOnClickListener(this)
+        val butAC: Button = findViewById(R.id.acBut)
+        butAC.setOnClickListener(this)
+        val butPlusMinus: Button = findViewById(R.id.plusMinusBut)
+        butPlusMinus.setOnClickListener(this)
+        val butPercent: Button = findViewById(R.id.percBut)
+        butPercent.setOnClickListener(this)
+        val butDivision: Button = findViewById(R.id.divisionBut)
+        butDivision.setOnClickListener(this)
+        val butMultiplication: Button = findViewById(R.id.multBut)
+        butMultiplication.setOnClickListener(this)
+        val butMinus: Button = findViewById(R.id.minusBut)
+        butMinus.setOnClickListener(this)
+        val butPlus: Button = findViewById(R.id.plusBut)
+        butPlus.setOnClickListener(this)
+        val butEqual: Button = findViewById(R.id.equalBut)
+        butEqual.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+
+        when(v?.id){
+            R.id.oneBut -> print("one")
+            else -> print("no button")
+        }
     }
 }
